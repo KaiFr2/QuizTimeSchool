eb032f8dc4b9d119ca27874624f2bd04c17d10ce
Dogukan <564937@edu.rocmn.nl>
2023-05-31 11:45:49 +0200

    Datagrid toegevoegd / style changes toegevoegd daarna ook nog nieuwe functies toegevoegd

--- QuizTime/Class/vragenopslaan.cs
+++ QuizTime/Class/vragenopslaan.cs
@@ -8,10 +8,11 @@ namespace QuizTime.Class
 {
     internal class vragenopslaan
     {
-        public int quizid;
-
+        public int quizid { get; set; }
         public int VraagID { get; set; }
-        public string vraag1 {get;set;}
+
+        public string title { get; set; }
+        public string vraag1 { get; set; }
         public string antwoord1 { get; set; }
         public string antwoord2 { get; set; }
         public string antwoord3 { get; set; }
--- QuizTime/MainWindow.xaml.cs
+++ QuizTime/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using QuizTime.Class;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,9 @@ namespace QuizTime
         private string checked_answer = "";
         private int currentID = 1;
         public quizopslaan newQuiz;
+        int quizID;
+        string quizzz;
+        int selectid;
 
         public MainWindow()
         {
@@ -33,25 +37,48 @@ namespace QuizTime
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //start scherm
             Start.Visibility = Visibility.Hidden;
-            mode.Visibility = Visibility.Visible;
+            homescreen.Visibility = Visibility.Visible; 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btnspeellijst_Click(object sender, RoutedEventArgs e)
         {
-            mode.Visibility = Visibility.Hidden;
-            Speel.Visibility = Visibility.Visible;
+            
+            homescreen.Visibility = Visibility.Hidden;
+            Kieslijst.Visibility = Visibility.Visible;
+            //leest het json file en maakt er een datagrid van
+            var SaveDataList = json.ReadSavedDataFile();
+            vragenopslaan vragenopslaan = new vragenopslaan();
+
+            dgAllQuizzes.ItemsSource = null;
+            dgAllQuizzes.ItemsSource = SaveDataList;
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void Maakquiz_Click(object sender, RoutedEventArgs e)
         {
-            mode.Visibility = Visibility.Hidden;
+            homescreen.Visibility = Visibility.Hidden;
             maaklijst.Visibility = Visibility.Visible;
+            currentID = 1;
+
+            //cleared alle textboxes
+            TitleQTB.Clear();
+            Beschrijving.Clear();
+            tijd.Clear();
+            Vraag1.Clear();
+            Antwoord1.Clear();
+            Antwoord2.Clear();
+            Antwoord3.Clear();
+            Antwoord4.Clear();
         }
 
         public void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            newQuiz = new quizopslaan(0, TitleQTB.Text, Beschrijving.Text, tijd.Text);
+            //quiz id
+            quizzz = File.ReadAllText("count.txt");
+            quizID = int.Parse(quizzz);
+            //opslaan van de quiz
+            newQuiz = new quizopslaan(quizID, TitleQTB.Text, Beschrijving.Text, tijd.Text);
 
             maaklijst.Visibility = Visibility.Hidden;
             maaklijst2.Visibility = Visibility.Visible;
@@ -59,9 +86,12 @@ namespace QuizTime
 
         private void Opslaan_Click(object sender, RoutedEventArgs e)
         {
+            //opslaan van de vragen
+            quizID++;
+            File.WriteAllText("count.txt", quizID.ToString());
             List<vragenopslaan> listQuizVragen = new List<vragenopslaan>();
 
-            listQuizVragen.Add(new vragenopslaan {quizid = newQuiz.iD, VraagID = currentID ,vraag1 = Vraag1.Text, antwoord1 = Antwoord1.Text, antwoord2 = Antwoord2.Text, antwoord3 = Antwoord3.Text, antwoord4 = Antwoord4.Text, check = checked_answer });
+            listQuizVragen.Add(new vragenopslaan {quizid = newQuiz.iD, VraagID = currentID, title = TitleQTB.Text  ,vraag1 = Vraag1.Text, antwoord1 = Antwoord1.Text, antwoord2 = Antwoord2.Text, antwoord3 = Antwoord3.Text, antwoord4 = Antwoord4.Text, check = checked_answer });
 
             currentID++; // VraagID
 
@@ -70,11 +100,16 @@ namespace QuizTime
             json.WriteDataToFile(SaveDataList);
 
             var list = json.ReadSavedDataFile();
-        }   
+
+            MessageBox.Show("Vraag opgeslagen", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+        }
 
 
         private void onCheckBoxCheck(object sender, RoutedEventArgs e)
         {
+            //checkbox opslaan van de goede antwoord
             Check1.IsChecked = false;
             Check2.IsChecked = false;
             Check3.IsChecked = false;
@@ -89,11 +124,44 @@ namespace QuizTime
 
         private void Volgende_Click(object sender, RoutedEventArgs e)
         {
+            //clear knop
             Vraag1.Clear();
             Antwoord1.Clear();
             Antwoord2.Clear();
             Antwoord3.Clear();
             Antwoord4.Clear();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            maaklijst2.Visibility = Visibility.Hidden;
+            homescreen.Visibility = Visibility.Visible;
+        }
+
+        private void btnbewerklijst_Click(object sender, RoutedEventArgs e)
+        {
+            //bewerk pagina WIP
+            homescreen.Visibility = Visibility.Hidden;
+            Kieslijst.Visibility = Visibility.Visible;
+
+            var SaveDataList = json.ReadSavedDataFile();
+            vragenopslaan vragenopslaan = new vragenopslaan();
+
+            dgAllQuizzes.ItemsSource = null;
+            dgAllQuizzes.ItemsSource = SaveDataList;
+        }
+
+        private void playbutton(object sender, RoutedEventArgs e)
+        {
+            //knop om quiz te spelen
+            Kieslijst.Visibility = Visibility.Hidden;
+            Speel.Visibility = Visibility.Visible;
+
+            var button = sender as Button;
+
+            int tempquizid = (int)button.CommandParameter;
+
+            selectid = tempquizid;
         }
     }
 }
